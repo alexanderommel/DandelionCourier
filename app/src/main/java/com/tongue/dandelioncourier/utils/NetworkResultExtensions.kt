6e3ce409c1f46c027b1fd024e176bca6f5e8f732
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-09-17 22:35:05 -0500

    Create NetworkResultExtensions.kt

---
+++ app/src/main/java/com/tongue/dandelioncourier/utils/NetworkResultExtensions.kt
@@ -0,0 +1,27 @@
+package com.tongue.dandelioncourier.utils
+
+import com.tongue.dandelioncourier.data.network.NetworkResult
+
+suspend fun <T : Any> NetworkResult<T>.onSuccess(
+    executable: suspend (T) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Success<T>) {
+        executable(payload)
+    }
+}
+
+suspend fun <T : Any> NetworkResult<T>.onError(
+    executable: suspend (code: Int, message: String?) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Error<T>) {
+        executable(httpCode, message)
+    }
+}
+
+suspend fun <T : Any> NetworkResult<T>.onException(
+    executable: suspend (e: Throwable) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Exception<T>) {
+        executable(e)
+    }
+}
